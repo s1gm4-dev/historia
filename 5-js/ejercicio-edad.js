bdc4eaa39ca35c9573cbf8de6e8f1f9afc739e3a
s1gm4-dev <diegocursogranada@gmail.com>
2025-10-07 12:56:41 +0200

    Add files via upload

---
+++ 5-js/ejercicio-edad.js
@@ -0,0 +1,11 @@
+/*
+// Pedir al usuario los datos
+let $anioActual = prompt("Ingresa el año actual:");
+let $anioNacimiento = prompt("Ingresa tu año de nacimiento:");
+
+// Calcular la edad (conversión a número)
+let $edad = Number($anioActual) - Number($anioNacimiento);
+
+// Mostrar la edad en el navegador
+document.write("Tu edad es: " + $edad + " años.");
+*/
